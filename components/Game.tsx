e3b760c68d2ff90d1a9b9baa49316a7dfa26ab0c
Israel Matos <15269860+izzydoesit@users.noreply.github.com>
2022-05-20 06:21:34 -0400

    Add Game view component

---
+++ components/Game.tsx
@@ -0,0 +1,10 @@
+import { View } from "./Themed"
+import { Button } from "react-native"
+
+export default function Game() {
+  return (
+    <View>
+      <Button title="Exit" />
+    </View>
+  )
+}
