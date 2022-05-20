ff8e32c1bebbc1730b8ecd514d32fedf08eca63f
Israel Matos <15269860+izzydoesit@users.noreply.github.com>
2022-05-20 13:06:12 -0400

    Move game logic to Game component

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,10 +1,51 @@
+import React, { useState } from "react"
 import { View } from "./Themed"
 import { Button } from "react-native"
+import Table from "./Table"
+import PlayerChoices from "./PlayerChoices"
+import { HandClass, CORRECT_MOVES, Option, POSITIONS } from "../util/GAME_DATA"
+import getRandomNumber from "../util/dealer_helpers"
 
 export default function Game() {
+  const [position, setPosition] = useState("")
+  const [playerChoice, setPlayerChoice] = useState(Option.NOT_SELECTED)
+  const [result, setResult] = useState("")
+  const [insight, setInsight] = useState("")
+  const [playerHand, setPlayerHand] = useState([])
+
+  const assignNewPosition = (): void => {
+    const randomNum: number = getRandomNumber(POSITIONS.length)
+    setPosition(POSITIONS[randomNum])
+  }
+
+  const checkPlaySelection = (): void => {
+    const handClass: HandClass = playerHand
+    // TODO: add this.player.position.seat to comparison
+    if (playerChoice === CORRECT_MOVES[handClass]) {
+      setResult("WIN")
+    } else {
+      setResult("LOSE")
+    }
+  }
+
+  const dealNewHand = (): void => {
+    setResult("")
+    setInsight("")
+    setPlayerChoice(Option.NOT_SELECTED)
+    setPlayerHand([])
+    setPosition("")
+
+    setTimeout(() => {
+      assignNewPosition()
+      setPlayerHand(deck.dealHand())
+    }, 7000)
+  }
+
   return (
     <View>
       <Button title="Exit" />
+      <Table result={result} insight={insight}></Table>
+      <PlayerChoices></PlayerChoices>
     </View>
   )
 }
--- components/Table.tsx
+++ components/Table.tsx
@@ -1,48 +1,25 @@
+import { View, Text } from "./Themed"
+import PlayerHand from "./PlayerHand"
+// import { useState } from "react"
 // import getRandomNumber from "../util/dealer_helpers"
-import { CORRECT_MOVES, HandClass, Option } from "../util/GAME_DATA"
-import Deck from "./Deck"
-import Hand from "./Hand"
-import Player from "./Player"
+//
+// import Deck from "./Deck"
+// import Hand from "./Hand"
+// import Player from "./Player"
 
-export default class Table {
+export default function Table({
+  result,
+  insight,
+}: {
   result: string
   insight: string
-  player: Player
-  deck: Deck
-
-  constructor(player: Player) {
-    this.deck = new Deck()
-    this.player = player
-    this.result = ""
-    this.insight = ""
-  }
-
-  checkPlaySelection(): void {
-    const handClass: HandClass = this.player.hand.strength
-    // TODO: add this.player.position.seat to comparison
-    if (this.player.selection === CORRECT_MOVES[handClass]) {
-      this.result = "WIN"
-    } else {
-      this.result = "LOSE"
-    }
-  }
-
-  displayResult(): void {
-    if (this.result && this.result.length > 0) {
-      console.log("RESULT: ", this.result)
-    }
-  }
-
-  resetTable(): void {
-    this.result = ""
-    this.insight = ""
-    this.player.selection = Option.NOT_SELECTED
-    this.player.hand = new Hand()
-    this.player.position.clearPosition()
-
-    setTimeout(() => {
-      this.player.position.assignNewPosition()
-      this.player.hand = this.deck.dealHand()
-    }, 7000)
-  }
+}) {
+  return (
+    <View>
+      <Text>This is a TABLE</Text>
+      <Text>{result}</Text>
+      <Text>{insight}</Text>
+      <PlayerHand />
+    </View>
+  )
 }
