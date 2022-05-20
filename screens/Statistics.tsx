0d5a9c030c42b7889af33556b9fe6f89d327aeef
Israel Matos <15269860+izzydoesit@users.noreply.github.com>
2022-05-20 06:24:16 -0400

    Add Statistics screen

--- screens/Statistics.tsx
+++ screens/Statistics.tsx
@@ -1,22 +1,18 @@
-import { StatusBar } from "expo-status-bar"
-import { Platform, StyleSheet } from "react-native"
+import { StyleSheet } from "react-native"
 
 import EditScreenInfo from "../components/GameChoiceInfo"
 import { Text, View } from "../components/Themed"
 
-export default function ModalScreen() {
+export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>Statistics</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+      <Text>Some statistical data...</Text>
     </View>
   )
 }
