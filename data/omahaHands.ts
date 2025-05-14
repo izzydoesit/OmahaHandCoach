fa3ee793fd54cf106fe2d6c15f4e440ec936bb02
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-05-13 23:28:36 -0400

    Add list of omaha hands to data

---
+++ data/omahaHands.ts
@@ -0,0 +1,12 @@
+
+export const omahaHands = [
+  { id: 1, name: "AA", description: "Pair of Aces" },
+  { id: 2, name: "KK", description: "Pair of Kings" },
+  { id: 3, name: "QQ", description: "Pair of Queens" },
+  { id: 4, name: "JJ", description: "Pair of Jacks" },
+  { id: 5, name: "AK", description: "Ace-King" },
+  { id: 6, name: "AQ", description: "Ace-Queen" },
+  { id: 7, name: "KQ", description: "King-Queen" },
+  { id: 8, name: "JT", description: "Jack-Ten" },
+  // Add more hands as needed
+];
