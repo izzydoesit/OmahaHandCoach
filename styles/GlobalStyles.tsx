cbcef827ad991509f5a7798ef047cabe36f8f4b0
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-05-13 23:23:37 -0400

    Add global styles

---
+++ styles/GlobalStyles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+	button: {
+		backgroundColor: "#4CAF50",
+		padding: 10,
+		marginTop: 20,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+});
